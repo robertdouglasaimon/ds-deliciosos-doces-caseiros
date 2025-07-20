315930e80dbeda72c96387eb5649089165d68e72
Robert Douglas <robertdouglasaimon@gmail.com>
2025-07-19 23:37:45 -0300

    Atualização
    - O rodapé agora exibe o ano atual automaticamente e inclui o CNPJ da empresa.
    
    - Isso melhora a precisão e a conformidade do aviso de direitos autorais.

--- doces-caseiros/src/Componentes/Footer/Footer.js
+++ doces-caseiros/src/Componentes/Footer/Footer.js
@@ -2,11 +2,14 @@ import React from "react";
 import "./Footer.css";
 
 const Footer = () => {
+  // Função para atualizar o ano automaticamente.
+  const year = new Date().getFullYear();
+
   return (
     <footer className="custom-footer">
         <p>
-            <b>© 2025 <a href="https://www.instagram.com/ds_doces_artesanais?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw==">DS Doces Artesanais Caseiros</a>. Todos os direitos reservados.</b><br/>
-            <b>Desenvolvido por <a href="https://github.com/robertdouglasaimon">Robert Douglas</a></b>
+            <b>© {year} <a href="https://www.instagram.com/ds_doces_artesanais?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw==">DS Doces Artesanais Caseiros</a>. CNPJ:59.900.451/0001-28.<br/> Todos os direitos reservados.</b>
+            <b> Desenvolvido por <a href="https://github.com/robertdouglasaimon">Robert Douglas</a></b>
         </p>
     </footer>
   );
